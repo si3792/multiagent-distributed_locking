76ef8e9f2b21af5c7013fc9859efaede0ddca101
Satia Herfert <sherfert@tzi.de>
2014-03-05 10:37:37 +0100

    Initial commit

---
+++ src/Dummy.hpp
@@ -0,0 +1,20 @@
+#ifndef _DUMMYPROJECT_DUMMY_HPP_
+#define _DUMMYPROJECT_DUMMY_HPP_
+
+#include <iostream>
+
+namespace ricart_agrawala
+{
+	class DummyClass
+	{
+		public: 
+			/**
+			* Print a welcome to stdout
+			* \return nothing
+			*/
+			void welcome();
+	};
+
+} // end namespace ricart_agrawala
+
+#endif // _DUMMYPROJECT_DUMMY_HPP_
---
+++ src/Main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <ricart_agrawala/Dummy.hpp>
+
+int main(int argc, char** argv)
+{
+	ricart_agrawala::DummyClass dummyClass;
+	dummyClass.welcome();
+
+	return 0;
+}
